973b4eb83a64695823b6b6fae2f4a2f249bb12eb
Anna <anna.yanova@gmail.com>
2016-03-30 12:02:08 +0100

    First Exercise from Chapter 2

---
+++ how_many_hours.rb
@@ -0,0 +1,22 @@
+# def hours_in_year
+ 
+#  "#{24 * 365} hours or #{24 * 366} in a leap year"
+
+# end
+
+# puts hours_in_year
+
+
+def hours_in_year
+   year = Time.now.year
+   leap_year = (year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0)
+   # days = 365
+   # days += 1 if leap_year
+   # days * 24
+   24 * 365 + (leap_year ? 24 : 0) # condition ? value_if_true : value_if_false
+end
+
+puts hours_in_year
+
+
+
