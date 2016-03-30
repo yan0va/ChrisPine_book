cc92f44ca2e2e2ffdff8cef13318ba23734c8860
Anna <anna.yanova@gmail.com>
2016-03-30 16:00:42 +0100

    Third Exercise from Chapter 6

---
+++ table_of_contents.rb
@@ -0,0 +1,10 @@
+title = "Table of Contents".center(60)
+tr1   = "Chapter 1:  Getting Started".ljust(30) + "page  1".rjust(30)
+tr2   = "Chapter 2:  Numbers".ljust(30)         + "page  9".rjust(30)
+tr3   = "Chapter 3:  Letters".ljust(30)         + "page 13".rjust(30) 
+
+puts title
+puts " "
+puts tr1
+puts tr2
+puts tr3
