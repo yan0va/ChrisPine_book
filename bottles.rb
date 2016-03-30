495a61981b2b6cd04a971ee10834eff4904cb619
Anna Yanova <anna.yanova@gmail.com>
2016-03-30 16:14:51 +0100

    Update bottles.rb

--- bottles.rb
+++ bottles.rb
@@ -11,7 +11,7 @@ if    n == 2
       puts "#{n} bottles of beer on the wall, #{n} bottles of beer"
       n = n - 1
       puts "Take one down and pass it around, #{n} bottle of beer on the wall."
-	  n < 1
+      n < 1
       puts "#{n} bottle of beer on the wall, #{n} bottle of beer"
       puts "Take one down and pass it around, no more bottles of beer on the wall."
 end      
