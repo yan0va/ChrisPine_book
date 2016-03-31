5a780094779a8e1a120b61cd0772cdec17908726
Anna Yanova <anna.yanova@gmail.com>
2016-03-31 12:00:04 +0100

    Update grandma_extended.rb

--- grandma_extended.rb
+++ grandma_extended.rb
@@ -4,15 +4,15 @@ bye_count = 0
 while true
 	reply = gets.chomp
 	if   reply == "BYE"
-		 bye_count += 1
+	     bye_count += 1
 	else bye_count = 0
 	end
 	break if bye_count >= 3
 
 	if  reply != reply.upcase
-		puts "HUH?! SPEAK UP, DEAR!!!"
+	    puts "HUH?! SPEAK UP, DEAR!!!"
 	else
-		puts "NOT SINCE #{rand(1930..1950)}!!!"
+	    puts "NOT SINCE #{rand(1930..1950)}!!!"
 	end
 end
 
