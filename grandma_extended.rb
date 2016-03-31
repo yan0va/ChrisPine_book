4d7d43a940a2e55bb5869beb8f93ed38e4dfc309
Anna <anna.yanova@gmail.com>
2016-03-31 11:57:44 +0100

    Third Exercise from Chapter 7

---
+++ grandma_extended.rb
@@ -0,0 +1,23 @@
+puts "HEllO DEAR! IT'S YOUR NANNA!"
+bye_count = 0
+
+while true
+	reply = gets.chomp
+	if   reply == "BYE"
+		 bye_count += 1
+	else bye_count = 0
+	end
+	break if bye_count >= 3
+
+	if  reply != reply.upcase
+		puts "HUH?! SPEAK UP, DEAR!!!"
+	else
+		puts "NOT SINCE #{rand(1930..1950)}!!!"
+	end
+end
+
+puts "BYE SWEETIE!"
+
+
+
+
