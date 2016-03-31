4528878c9860303863ed821c3180a4f6cb762895
Anna <anna.yanova@gmail.com>
2016-03-31 11:39:00 +0100

    Second Exercise from Chapter 7

---
+++ grandma.rb
@@ -0,0 +1,19 @@
+puts "HEllO DEAR! IT'S YOUR NANNA!"
+
+while true
+	reply = gets.chomp
+	if  reply == "BYE"
+		puts "BYE SWEETIE!"
+	break
+    end
+
+	if  reply != reply.upcase
+		puts "HUH?! SPEAK UP, DEAR!!!"
+	else
+		puts "NOT SINCE #{rand(1930..1950)}!!!"
+	end
+end
+
+
+
+
